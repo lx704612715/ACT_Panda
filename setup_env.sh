77df7241b0dab147e7cec1132a84ae174cf20c95
Xing Li <xing.li@tu-berlin.de>
2024-07-11 20:49:06 +0200

    bug in setup_env

--- setup_env.sh
+++ setup_env.sh
@@ -6,4 +6,5 @@ absolute_dir=$(realpath "$script_dir")
 
 # Set PYTHONPATH to include the script's directory (or any specific subdirectory)
 export PYTHONPATH=$PYTHONPATH:$absolute_dir
-echo "ACT_PROJECT_DIR set to: $absolute_dir"
+export ACT_PROJECT_DIR="$absolute_dir"
+echo "ACT_PROJECT_DIR set to: $ACT_PROJECT_DIR"
