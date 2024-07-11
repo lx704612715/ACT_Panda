e222cb63647264488035fc8189bb8b7f38ae570e
Xing Li <xing.li@tu-berlin.de>
2024-07-11 20:47:18 +0200

    bug in setup_env

--- setup_env.sh
+++ setup_env.sh
@@ -5,6 +5,5 @@ script_dir=$(dirname "${BASH_SOURCE[0]}")
 absolute_dir=$(realpath "$script_dir")
 
 # Set PYTHONPATH to include the script's directory (or any specific subdirectory)
-export PYTHONPATH="$absolute_dir:$PYTHONPATH"
-export ACT_PROJECT_DIR="$absolute_dir"
-echo "ACT_PROJECT_DIR set to: $ACT_PROJECT_DIR"
+export PYTHONPATH=$PYTHONPATH:$absolute_dir
+echo "ACT_PROJECT_DIR set to: $absolute_dir"
